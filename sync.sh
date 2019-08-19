9ecf3ee81ad1d9c6dab2f5a198519f4635cd9a03
Kyoz <banminkyoz@gmail.com>
2019-08-19 22:29:23 +0700

    update: add arch dotfiles

--- sync.sh
+++ sync.sh
@@ -65,3 +65,78 @@ log "\nSync config from your PC to this project...\n"
 } || {
   log_error "Can't sync zsh theme"
 }
+
+################################################################################
+###########################         ARCH         ###############################
+################################################################################
+
+# If not arch, don't sync or install those below files
+if [ ! $(uname -r | grep 'ARCH') ]; then
+  exit
+fi
+
+{
+  cp $X_DEFAULT_PATH .Xdefaults && log_success "Synced .Xdefaults"
+} || {
+  log_error "Can't sync .Xdefaults"
+}
+
+{
+  cp $X_INITRC_PATH .xinitrc && log_success "Synced .xinitrc"
+} || {
+  log_error "Can't sync .xinitrc"
+}
+
+{
+  cp $X_MODMAP_PATH .Xmodmap && log_success "Synced .Xmodmap"
+} || {
+  log_error "Can't sync .Xmodmap"
+}
+
+{
+  cp $X_PROFILE_PATH .xprofile && log_success "Synced .xprofile"
+} || {
+  log_error "Can't sync .xprofile"
+}
+
+{
+  cp $X_RESOURCE_PATH .Xresources && log_success "Synced .Xresources"
+} || {
+  log_error "Can't sync .Xresources"
+}
+
+{
+  cp $SXHKDRC_PATH .config/sxhkd/sxhkdrc && log_success "Synced .sxhkdrc"
+} || {
+  log_error "Can't sync .sxhkdrc"
+}
+
+{
+  cp $I3_PATH .config/i3/config && log_success "Synced i3 config"
+} || {
+  log_error "Can't sync i3 config"
+}
+
+{
+  cp $I3_BLOCK_PATH .config/i3blocks/config && log_success "Synced i3 blocks config"
+} || {
+  log_error "Can't sync i3 blocks config"
+}
+
+{
+  cp $LOCAL_I3_PATH .local/bin/i3/* && log_success "Synced local i3 scripts"
+} || {
+  log_error "Can't sync local i3 scripts"
+}
+
+{
+  cp $LOCAL_I3_BLOCKS_PATH .local/bin/i3blocks/* && log_success "Synced local i3blocks scripts"
+} || {
+  log_error "Can't sync local i3blocks scripts"
+}
+
+{
+  cp $LOCAL_TOOLS_PATH .local/bin/tools/* && log_success "Synced local tool scripts"
+} || {
+  log_error "Can't sync local tool scripts"
+}
