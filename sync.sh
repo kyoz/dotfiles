9ef9602890ec61747ddea936401879dbf4cbb0b6
Kyoz <banminkyoz@gmail.com>
2020-03-14 16:12:19 +0700

    add yabai & skhd configs

--- sync.sh
+++ sync.sh
@@ -17,24 +17,26 @@ copy $ALACRITTY_PATH .alacritty.yml
 copy $HYPER_PATH .hyper.js
 copy $ZSH_THEME_PATH purify.zsh-theme
 
-################################################################################
-###########################         ARCH         ###############################
-################################################################################
-
-# If not linux, don't sync or install those below files
-if [[ "$OSTYPE" != "linux-gnu" ]]; then
-  exit
+# If os is OSX, then sync those below files
+if [[ "$OSTYPE" == *"darwin"* ]]; then
+  copy $YABAI_PATH .config/yabai/yabairc
+  copy $SKHD_PATH .config/skhd/skhdrc
 fi
 
-copy $X_DEFAULT_PATH .Xdefaults
-copy $X_INITRC_PATH .xinitrc
-copy $X_MODMAP_PATH .Xmodmap
-copy $X_PROFILE_PATH .xprofile
-copy $X_RESOURCE_PATH .Xresources
-copy $SXHKDRC_PATH .config/sxhkd/sxhkdrc
-copy $I3_PATH .config/i3/config
-copy $I3_BLOCK_PATH .config/i3blocks/config
-copy_folder $LOCAL_I3_PATH .local/bin
-copy_folder $LOCAL_I3_BLOCKS_PATH .local/bin
-copy_folder $LOCAL_TOOLS_PATH .local/bin
+
+# If os is linux, then sync those below files
+if [[ "$OSTYPE" == *"linux"* ]]; then
+  copy $X_DEFAULT_PATH .Xdefaults
+  copy $X_INITRC_PATH .xinitrc
+  copy $X_MODMAP_PATH .Xmodmap
+  copy $X_PROFILE_PATH .xprofile
+  copy $X_RESOURCE_PATH .Xresources
+  copy $SXHKDRC_PATH .config/sxhkd/sxhkdrc
+  copy $I3_PATH .config/i3/config
+  copy $I3_BLOCK_PATH .config/i3blocks/config
+  copy_folder $LOCAL_I3_PATH .local/bin
+  copy_folder $LOCAL_I3_BLOCKS_PATH .local/bin
+  copy_folder $LOCAL_TOOLS_PATH .local/bin
+fi
+
 
